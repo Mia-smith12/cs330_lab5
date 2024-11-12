45538f7252dfa6c5fe1f5b5d366c9f38a7c50335
Mia-smith12 <miabeth1212@gmail.com>
2024-11-11 21:44:14 -0600

    Update asgn6.s

--- asgn6.s
+++ asgn6.s
@@ -1,10 +1,12 @@
 .data  
-
- # start of data section
+# start of data section
 # put any global or static variables here
 
 .section .rodata
-printString: .string "The answer is %d\n"
+printString: .string "1. Result: %d\n"
+printString2: .string "2. Result: %d\n"
+printString3: .string "3. Result: %d\n"
+
 # start of read-only data section
 # constants here, such as strings
 # modifying these during runtime causes a segmentation fault, so be cautious!
@@ -13,33 +15,64 @@ printString: .string "The answer is %d\n"
 .text    
 # start of text /code
 # everything inside .text is read-only, which includes your code!
-.global _main        # required, tells gcc where to begin execution
+.global main        
+# required, tells gcc where to begin execution
 # === functions here ===
 
-_main:               # start of main() function
+main:             
+# start of main() function
 # preamble
 pushq %rbp
 movq %rsp, %rbp
 
 # === main() code here ===
 
-# setting up a & b
-movq $2, %rbx  #a = rbx (2)
-movq $3, %rcx  #b = rcx (3)
+# setting up a & b - hardcoding
+movq $6, %rbx  #a = rbx (4)
+movq $2, %r12  #b = r12 (3)
 
 # a * 5
 movq $5, %rax
 mul %rbx  # a(rbx) * 5(rax) in rax 
 
-# print answer
-movq $printString, %rdi  
-movq %rax, %rsi          
+# print answer (2 * 5)
+movq $printString, %rdi    #rdi register for format string 
+movq %rax, %rsi          #rsi for print value
 xorq %rax, %rax         
 Call printf
 
+# (a + b) - (a/b)
+movq %r12, %r11  # b in r11    c: r11 = b
+addq %rbx, %r11  # a + b in r11   c: r11+= a
 
+movq %rbx, %rax    # a in rax  c: rax = a
+cqto             #sign extends   rdx:rax
+idivq %r12       #a/b (rax / rcx) in rax   
+subq %rax, %r11 #(a+b) - (a/b) in r11
 
+#print answer
+movq $printString2, %rdi    
+movq %r11, %rsi          
+xorq %rax, %rax         
+Call printf
 
+# a = 6, b = 2
+
+# (a-b) + (a*b)
+movq %rbx, %r9  # a in r9
+movq %r12, %r10  # b in r10
+subq %r10, %r9  # a - b = r9
+ 
+movq %rbx, %rax  # a in rax
+mulq %r12   # a * b in rax
+
+addq %rax, %r9   # (a-b) + (a*b) in rax
+
+#print answer
+movq $printString3, %rdi    
+movq %r9, %rsi          
+xorq %rax, %rax         
+Call printf
 
 # clean up and return
 movq $0, %rax       # place return value in rax
